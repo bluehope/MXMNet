1d90f887720233c9dd3571f11e17934e93c4da34
Youngwoo Cho <cyw314@gmail.com>
2022-09-01 23:41:43 +0900

    dbug

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ module purge
 module load singularity/3.9.7
 module load htop nvtop
 module load gcc/10.2.0
-module load cuda/10.2
 module load cudampi/openmpi-4.1.1
+module load cuda/10.2
 module list
 
 conda activate mxmnet
