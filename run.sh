bf43e3ced3260c654c443d9a99e93bb21ed1edef
Youngwoo Cho <cyw314@gmail.com>
2022-09-01 23:43:51 +0900

    debug

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ module purge
 module load singularity/3.9.7
 module load htop nvtop
 module load gcc/10.2.0
+module load cuda/11.4
 module load cudampi/openmpi-4.1.1
 module load cuda/10.2
 module list
