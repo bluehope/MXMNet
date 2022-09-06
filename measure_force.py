841cccfe8bff41e90e05e8b60fc0f10d64caa981
Youngwoo Cho <cyw314@gmail.com>
2022-09-06 16:33:02 +0900

    Update measure_force.py

--- measure_force.py
+++ measure_force.py
@@ -1,15 +1,61 @@
 import os
 import sys
+import argparse
 import numpy as np
 from typing import List, Dict, Tuple, Set, Union, Optional, Any, Callable
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.autograd
+from torch_geometric.data import DataLoader
 
 from utils import EMA
 from model import MXMNet, Config
 from qm9_dataset import QM9
+
+class MyTransform(object):
+
+    def __init__(self,
+                target: int) -> None:
+
+        if target in [7, 8, 9, 10]:
+            
+            target = target + 5
+
+        self.target = target
+
+    def __call__(self,
+                data):
+
+        data.y = data.y[:, self.target]
+
+        return data
+
+
+def measure_force(model: nn.Module,
+                dataloader: DataLoader) -> None:
+
+    count_atom = 0
+    force_sum = 0
+
+    for data in dataloader:
+
+        data = data.to('cuda')
+        data.pos = data.pos.requires_grad_(True)
+        data.pos.retain_grad()
+
+        output = model(data)
+        force = torch.autograd.grad(output,
+                                    data.pos,
+                                    grad_outputs = torch.ones_like(output).to('cuda'),
+                                    create_graph = True)[0]
+
+        num_atom = data.batch.size(0)
+        count_atom += num_atom
+        force_sum += torch.sum(torch.sqrt(torch.sum(force ** 2, dim = -1))).item()
+
+    return force_sum / count_atom
 
 
 def main(args: Dict[str, Any]) -> None:
@@ -19,7 +65,24 @@ def main(args: Dict[str, Any]) -> None:
     model.load_state_dict(torch.load(args['model_checkpoint'], map_location = 'cpu'))
     model = model.to('cuda')
 
-    
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.', 'data', 'QM9')
+    dataset = QM9(path, transform = MyTransform(args['target'])).shuffle()
+    print('# of graphs:', len(dataset))
+
+    train_dataset = dataset[:110000]
+    val_dataset = dataset[110000:120000]
+    test_dataset = dataset[120000:]
+
+    train_loader = DataLoader(train_dataset, batch_size = args['batch_size'], shuffle = False)
+    test_loader = DataLoader(test_dataset, batch_size = args['batch_size'], shuffle = False)
+    val_loader = DataLoader(val_dataset, batch_size = args['batch_size'], shuffle = False)
+
+    train_force = measure_force(model, train_loader)
+    test_force = measure_force(model, test_loader)
+    val_force = measure_force(model, val_loader)
+
+    print('[info] Model checkpoint: {}'.format(args['model_checkpoint']))
+    print('[info] Train force: {:.7f}, Test force: {:.7f}, Valid force: {:.7f}'.format(train_force, test_force, val_force))
 
 
 if __name__ == '__main__':
